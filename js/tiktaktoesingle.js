0e5b35b422d144c2836ed5779fb45ba856437b8a
Yangjas4 <Jasonyang602@gmail.com>
2021-11-25 02:53:56 -0500

    singleplayer

--- js/tiktaktoesingle.js
+++ js/tiktaktoesingle.js
@@ -6,24 +6,23 @@ function startGame() {
     //fades out the titlescreen and clears the contents to make room for the gameboard
     $(".titleScreen").fadeOut(1500, function () {
         $(".grid").empty();
-    });
-    assignPlayer();
-    var newMsg = document.createElement("div");
-    $(newMsg).attr("class", "tileIndicator");
-    if (player == "o") {
-        $(newMsg).append(" <h1> You are Os!</h1>");
-        console.log(newMsg);
-    } else {
-        $(newMsg).append(" <h1> You are Xs!</h1>");
-        console.log(newMsg);
-    }
-    $(newMsg).hide().appendTo(".grid").fadeIn(1000).delay(1000).fadeOut(1000);
-    //assigns player either Xs or Os and indicates which they've been assigned through a popup 
-    //which can be closed by clicking anywhere on the screen
 
+        //assigns player either Xs or Os and indicates which they've been assigned through a popup 
+        assignPlayer();
+        var newMsg = document.createElement("div");
+        $(newMsg).attr("class", "tileIndicator");
+        if (player == "o") {
+            $(newMsg).append(" <h1> You are <span>O</span>s!</h1>");
+
+        } else {
+            $(newMsg).append(" <h1> You are <span>X</span>s!</h1>");
+        }
+        $(newMsg).hide().appendTo(".grid").fadeIn(1000).delay(300).fadeOut(1000, function () {
+            $(".grid").empty();});
+    });
 
     //creates the gameboard
-    /*var newBox = document.createElement("button");
+    var newBox = document.createElement("button");
     var boxContainer = document.createElement("div");
     $(boxContainer).attr("id", "boxContainer");
 
@@ -42,6 +41,5 @@ function assignPlayer() {
         player = "o";
         cpu = "x";
     }
-    console.log(player);
-    console.log(playerRandom);
 }
+
