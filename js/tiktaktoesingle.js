138c13a479acd9c1c0bb3e781cd49eec4d792464
Yangjas4 <Jasonyang602@gmail.com>
2021-11-26 17:53:44 -0500

    game implementation

--- js/tiktaktoesingle.js
+++ js/tiktaktoesingle.js
@@ -1,27 +1,38 @@
 var player = null;
 var cpu = null;
+var winstreak = 0;
 
+//triggered by start game button
 function startGame() {
 
     //fades out the titlescreen and clears the contents to make room for the gameboard
     $(".titleScreen").fadeOut(1500, function () {
-        $(".grid").empty();
-
-        //assigns player either Xs or Os and indicates which they've been assigned through a popup 
-        assignPlayer();
-        var newMsg = document.createElement("div");
-        $(newMsg).attr("class", "tileIndicator");
-        if (player == "o") {
-            $(newMsg).append(" <h1> You are <span>O</span>s!</h1>");
-
-        } else {
-            $(newMsg).append(" <h1> You are <span>X</span>s!</h1>");
-        }
-        $(newMsg).hide().appendTo(".grid").fadeIn(1000).delay(300).fadeOut(1000, function () {
-            $(".grid").empty();});
+        initGame();
+        createGameBoard();
     });
+}
 
-    //creates the gameboard
+//restarts game
+function initGame() {
+    winstreak = 0;
+    $(".grid").empty();
+    assignPlayer();
+    var newMsg = document.createElement("div");
+    $(newMsg).attr("class", "tileIndicator");
+    if (player == "o") {
+        $(newMsg).append(" <h1> You are <span>O</span>s!</h1>");
+
+    } else {
+        $(newMsg).append(" <h1> You are <span>X</span>s!</h1>");
+    }
+    $(newMsg).hide().appendTo(".grid").fadeIn(1000).delay(300).fadeOut(1000, function () {
+        $(".grid").empty();
+        return;
+    });
+}
+
+//creates and appends the gameboard to .grid
+function createGameboard() {
     var newBox = document.createElement("button");
     var boxContainer = document.createElement("div");
     $(boxContainer).attr("id", "boxContainer");
@@ -32,6 +43,7 @@ function startGame() {
     //$(".grid").*/
 }
 
+//assigns player and cpu as x or o randomly
 function assignPlayer() {
     var playerRandom = Math.floor(Math.random() * 2);
     if (playerRandom == 0) {
