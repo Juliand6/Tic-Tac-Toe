af0d1a3061821ee8a00bb7b66c50c598ef3c691d
Yangjas4 <Jasonyang602@gmail.com>
2021-11-27 11:20:07 -0500

    creating gameboard elements

--- js/tiktaktoesingle.js
+++ js/tiktaktoesingle.js
@@ -1,6 +1,8 @@
 var player = null;
 var cpu = null;
 var winstreak = 0;
+var squares = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+var turn = true;
 
 //triggered by start game button
 function startGame() {
@@ -8,11 +10,10 @@ function startGame() {
     //fades out the titlescreen and clears the contents to make room for the gameboard
     $(".titleScreen").fadeOut(1500, function () {
         initGame();
-        createGameBoard();
     });
 }
 
-//restarts game
+//start new game
 function initGame() {
     winstreak = 0;
     $(".grid").empty();
@@ -27,21 +28,42 @@ function initGame() {
     }
     $(newMsg).hide().appendTo(".grid").fadeIn(1000).delay(300).fadeOut(1000, function () {
         $(".grid").empty();
-        return;
+        createGameboard();
     });
 }
 
 //creates and appends the gameboard to .grid
 function createGameboard() {
-    var newBox = document.createElement("button");
-    var boxContainer = document.createElement("div");
-    $(boxContainer).attr("id", "boxContainer");
-
-    for (var i = 0; i <= 9; i++) {
-        $(newBox).attr("onclick", "");
+    var newContainer = document.createElement("div");
+    $(newContainer).attr("id", "container");
+    $(".grid").append(newContainer);
+    for (var i = 0; i <= 8; i++) {
+        var newSquare = document.createElement("div");
+        $(newSquare).attr("class", "gridSquares");
+        $(newSquare).attr("id", "square" + i);
+        $(newSquare).attr("onclick", "squareClicked(" + i + ")");
+        $(newContainer).append(newSquare);
     }
-    //$(".grid").*/
 }
+
+
+//onclick event handler for individual squares on the gameboard
+function squareClicked(n) {
+    if (turn == false) {
+        return;
+    } else if (squares[n] == 0) {
+        var squareText = document.createElement("p");
+        if (player == "x") {
+            $(squareText).append("<p>X</p>");
+            squares[n]=1;
+        } else if (player == "o") {
+            $(squareText).append("<p>O</p>");
+            squares[n]=2;
+        }
+        $("#square"+n).append(squareText);
+    }
+}
+
 
 //assigns player and cpu as x or o randomly
 function assignPlayer() {
