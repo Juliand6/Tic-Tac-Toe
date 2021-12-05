41a475e73da7296ecd558f0f888fc6fa7eb6bcaf
Julian D’Alessandro <92184786+Juliand6@users.noreply.github.com>
2021-12-04 23:39:58 -0500

    Add files via upload

---
+++ js/tiktaktoehome.js
@@ -0,0 +1,9 @@
+//WEATHER POPUP
+function openWeather() {
+
+    document.getElementById("weather_container").style.display = "block";
+    }
+    function closeWeather() {
+        
+        document.getElementById("weather_container").style.display = "none";
+        }
